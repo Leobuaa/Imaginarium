4ba133ecf7c9b7e03def3e88562ad31f1c08cb2d
Leo Peng <pengzimaobuaa@163.com>
2016-05-25 09:56:53 +0800

    Downloading image background

---
+++ Imaginarium/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  Imaginarium
+//
+//  Created by Leo Peng on 5/24/16.
+//  Copyright © 2016 Leo Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+@property (strong, nonatomic) NSURL *imageURL;
+@end
---
+++ Imaginarium/ImageViewController.m
@@ -0,0 +1,77 @@
+//
+//  ImageViewController.m
+//  Imaginarium
+//
+//  Created by Leo Peng on 5/24/16.
+//  Copyright © 2016 Leo Peng. All rights reserved.
+//
+
+#import "ImageViewController.h"
+
+@interface ImageViewController () <UIScrollViewDelegate>
+@property (strong, nonatomic) UIImageView *imageView;
+@property (strong, nonatomic) UIImage *image;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollview;
+@end
+
+@implementation ImageViewController
+
+- (void)setScrollview:(UIScrollView *)scrollview {
+    _scrollview = scrollview;
+    _scrollview.minimumZoomScale = 0.2;
+    _scrollview.maximumZoomScale = 2.0;
+    _scrollview.delegate = self;
+    self.scrollview.contentSize = self.image ? self.image.size : CGSizeZero;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
+
+- (void)setImageURL:(NSURL *)imageURL {
+    _imageURL = imageURL;
+    //self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:self.imageURL]];
+    [self startDownloadingImage];
+}
+
+- (void)startDownloadingImage {
+    self.image = nil;
+    if (self.imageURL) {
+        NSURLRequest *request = [NSURLRequest requestWithURL:self.imageURL];
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+        NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request completionHandler:^(NSURL * _Nullable localfile, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            if (!error) {
+                if ([request.URL isEqual:self.imageURL]) {
+                    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:localfile]];
+                    [self performSelectorOnMainThread:@selector(setImage:) withObject:image waitUntilDone:NO];
+                }
+            }
+        }];
+        [task resume];
+    }
+}
+
+- (UIImageView *)imageView {
+    if (!_imageView) {
+        _imageView = [[UIImageView alloc] init];
+    }
+    
+    return _imageView;
+}
+
+- (UIImage *)image {
+    return self.imageView.image;
+}
+
+- (void)setImage:(UIImage *)image {
+    self.imageView.image = image;
+    [self.imageView sizeToFit];
+    self.scrollview.contentSize = self.image ? self.image.size : CGSizeZero;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.scrollview addSubview:self.imageView];
+}
+@end
